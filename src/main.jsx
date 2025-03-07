67144e81faa2ab517edfabc247cb70d81ce27506
BibashDahal1 <bibash98425@gmail.com>
2025-03-07 23:35:43 +0545

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'  // Add this import
+import {HashRouter} from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>  
+    <HashRouter>  
       <App />      
-    </AuthProvider>
+    </HashRouter>
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -4,37 +4,31 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/e-commerce.github.io/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   optimizeDeps: {
     include: ['jwt-decode']
   },
-  build: {
-    outDir: 'dist',
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'https://zd88bbhd-5000.inc1.devtunnels.ms',
+        changeOrigin: true,
+        secure: false
+      },
+      '/api': {
+        target: 'https://zd88bbhd-5000.inc1.devtunnels.ms',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     include: ['jwt-decode']
-//   },
-//   server: {
-//     proxy: {
-//       '/auth': {
-//         target: 'https://zd88bbhd-5000.inc1.devtunnels.ms',
-//         changeOrigin: true,
-//         secure: false
-//       },
-//       '/api': {
-//         target: 'https://zd88bbhd-5000.inc1.devtunnels.ms',
-//         changeOrigin: true,
-//         secure: false
-//       }
-//     }
-//   }
-// })
 
 
