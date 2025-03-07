6f448f20b9613d2c9e0ea107a7719b38ce1b8c13
Bhatta-om <bhattaom2077@gmail.com>
2025-03-07 20:49:35 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/e-commerce.github.io/', // Assuming this is your repository name
+  base: '/e-commerce.github.io/', // Your repository name
   optimizeDeps: {
     include: ['jwt-decode']
   }
